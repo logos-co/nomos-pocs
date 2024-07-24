a268129ee954b9d03f00299485e68d424bf92009
David Rusu <davidrusu.me@gmail.com>
2024-07-25 02:24:56 +0400

    pol: remove copied proof statements

--- proof_of_leadership/risc0/proof_statements/src/death_constraint.rs
+++
@@ -1,8 +0,0 @@
-use cl::{Nullifier, PtxRoot};
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
-pub struct DeathConstraintPublic {
-    pub nf: Nullifier,
-    pub ptx_root: PtxRoot,
-}
--- proof_of_leadership/risc0/proof_statements/src/lib.rs
+++ proof_of_leadership/risc0/proof_statements/src/lib.rs
@@ -1,4 +1,1 @@
-pub mod death_constraint;
-pub mod input;
-pub mod ptx;
 pub mod proof_of_leadership;
