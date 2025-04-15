83738f4495f49f14181a83bc1a2c957d87f88f29
Giacomo Pasini <g.pasini98@gmail.com>
2025-04-15 13:27:43 +0200

    remove leftover comment

--- sz-poc-offsite-2025/evm/processor/src/lib.rs
+++ sz-poc-offsite-2025/evm/processor/src/lib.rs
@@ -4,8 +4,7 @@ pub use executor_http_client::BasicAuthCredentials;
 use reqwest::Url;
 use reth_tracing::tracing::{info, error};
 use kzgrs_backend::{dispersal::Metadata, encoder::DaEncoderParams};
-// TODO: The logic to batch multiple of these blocks (or the transactions within them) and send them to DA and generate proofs is still missing. It will have to be added at the offsite.
-// This type does not support any recovery mechanism, so if the node is stopped, the state DB should be cleaned before starting again. The folder is specified by the `--datadir` option in the binary.
+
 pub struct Processor {
     da: NomosDa,
 }
