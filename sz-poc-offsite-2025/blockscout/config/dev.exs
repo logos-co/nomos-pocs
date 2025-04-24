523c8bdb1ed7c8b5915c718c3939a464a88d3c5d
Alejandro Cabeza Romero <alex93cabeza@gmail.com>
2025-04-24 17:06:44 +0200

    Blockscout listen to all http interfaces.

--- sz-poc-offsite-2025/blockscout/config/dev.exs
+++ sz-poc-offsite-2025/blockscout/config/dev.exs
@@ -15,3 +15,6 @@ config :logger, :account,
   level: :debug,
   path: Path.absname("logs/dev/account.log"),
   metadata_filter: [fetcher: :account]
+
+config :block_scout_web, BlockScoutWeb.Endpoint,
+  http: [ip: {0, 0, 0, 0}, port: 4000]
