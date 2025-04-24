35cba3a980bbcb181707512364e4c9f4d56ea679
Giacomo Pasini <21265557+zeegomo@users.noreply.github.com>
2025-04-24 12:42:22 +0200

    Off eth ref (#67)
    * dummy prover
    
    * delete binaries
    
    * absolute binary paths
    
    * revert processor change
    
    * improve prover with more idiomatic rust code
    
    * lightnode
    
    * lightnode
    
    * nomos types needed for client
    
    * credentials
    
    * basic http calls
    
    * cryptarchia info and block fetch loop
    
    * remove printlns
    
    * serve proofs through http interface
    
    * add missing deps
    
    * remove tracing initialization
    
    * check binary
    
    * fix logs
    
    * remove non-working code
    
    * fix
    
    ---------
    
    Co-authored-by: Petar Radovic <petar.radovic@gmail.com>

---
+++ sz-poc-offsite-2025/evm/lightnode/src/main.rs
@@ -0,0 +1,48 @@
+use clap::Parser;
+use evm_lightnode::proofcheck;
+use url::Url;
+use std::path::PathBuf;
+use std::error;
+use tracing_subscriber::{EnvFilter, fmt};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about = "Light Node validator")]
+struct Args {
+    #[clap(long, default_value = "info")]
+    log_level: String,
+
+    #[clap(long, default_value = "http://localhost:8546")]
+    rpc: Url,
+
+    #[clap(long, default_value = "http://localhost:8070")]
+    prover_url: Url,
+
+    #[clap(long)]
+    start_block: u64,
+
+    #[clap(long, default_value = "10")]
+    batch_size: u64,
+
+    #[clap(long)]
+    zeth_binary_dir: Option<PathBuf>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    let args = Args::parse();
+
+    let filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(&args.log_level));
+
+    fmt::fmt().with_env_filter(filter).with_target(false).init();
+
+    proofcheck::verify_proof(
+        args.start_block,
+        args.batch_size,
+        &args.rpc,
+        &args.prover_url,
+        &args.zeth_binary_dir.unwrap_or_else(|| std::env::current_dir().unwrap()).join("zeth-ethereum"),
+    ).await?;
+
+    Ok(())
+}
