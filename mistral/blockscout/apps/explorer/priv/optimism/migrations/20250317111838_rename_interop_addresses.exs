3c46c7321b924aa255b91aa5b978cab1bda470a0
Álex <alex93cabeza@gmail.com>
2025-05-14 13:02:41 +0200

    Remove uniswap frontend, uniswap's contracts and blockscout. (#73)

--- mistral/blockscout/apps/explorer/priv/optimism/migrations/20250317111838_rename_interop_addresses.exs
+++
@@ -1,8 +0,0 @@
-defmodule Explorer.Repo.Optimism.Migrations.RenameInteropAddresses do
-  use Ecto.Migration
-
-  def change do
-    rename(table(:op_interop_messages), :sender, to: :sender_address_hash)
-    rename(table(:op_interop_messages), :target, to: :target_address_hash)
-  end
-end
--- mistral/uniswap/contracts/README.md
+++
@@ -1,2 +0,0 @@
-Source: https://github.com/Ben-Haslam/uniswap-deploy-scripts 
-Commit: 759c56fd30b701b352f57f350c2c3f8727a368b7
